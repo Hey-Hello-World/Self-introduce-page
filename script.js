617c32f4904be07a4a82707678c53a0acb337d54
Hey-Hello-World <107197044+Hey-Hello-World@users.noreply.github.com>
2022-08-13 22:25:52 +0900

    [Week 1] 1주 차 미션

---
+++ script.js
@@ -0,0 +1,16 @@
+
+const hexcode = function(){
+    const hexNumbers = [0,1,2,3,4,5,6,7,8,9,'A','B','C','D','E','F'];
+    let result = "#";
+     
+    for(let i=0; i<6; i++){
+    const randomIndex = Math.floor(Math.random() * hexNumbers.length);
+    result += hexNumbers[randomIndex];
+    }
+    return result;
+}
+const btn = document.querySelector('button');
+
+btn.addEventListener('click', function(){
+    document.body.style.color = hexcode();
+})
